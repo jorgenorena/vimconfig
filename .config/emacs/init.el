4737380bb7d5eb475839924c29b510054b376613
root <root@localhost.localdomain>
2024-05-17 01:44:37 +0000

    bug fixes

---
+++ .config/emacs/init.el
@@ -0,0 +1,97 @@
+;;; init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Jorge
+;;
+;; Author: Jorge <jorge@jorge-XENIA-15>
+;; Maintainer: Jorge <jorge@jorge-XENIA-15>
+;; Created: mayo 16, 2024
+;; Modified: mayo 16, 2024
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/jorge/init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;; ---------------- EVIL MODE -----------------------
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ; This is optional, required for some packages
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1)
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package evil-surround
+  :after evil
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-leader
+  :after evil
+  :ensure t
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader "<SPC>"))
+
+(use-package evil-commentary
+  :after evil
+  :ensure t
+  :config
+  (evil-commentary-mode))
+
+(use-package evil-multiedit
+  :after evil
+  :ensure t
+  :config
+  (evil-multiedit-default-keybinds))
+
+(use-package evil-snipe
+  :after evil
+  :ensure t
+  :init
+  (evil-snipe-mode 1))  ; Globally enable evil-snipe
+
+
+(provide 'init)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-snipe evil-commentary evil-leader evil-surround evil-collection evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
